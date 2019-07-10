fc7bdc7200b28584419ac7f2692ec0445219812c
RRB79 <smiko@bk.ru>
2019-07-10 23:31:07 +0500

    hometask_2

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -4,16 +4,22 @@ import ru.skillbranch.devintensive.extensions.getFirstLetter
 import ru.skillbranch.devintensive.extensions.getTranlitLetter
 
 object Utils {
-    fun parseFullName(fullName: String?): Pair<String?, String?> {
-      //  var fullName: String?=fullName?.toRegex(" ")
-        val parts: List<String>? = fullName?.split(" ")
+    fun stringOrNull(anyText: String?): String? {
+        var sn = anyText.toString().replace(" ", "")
+        if (sn.length == 0) {
+            return null
+        }
+        return sn
+    }
 
+    fun parseFullName(fullName: String?): Pair<String?, String?> {
+        //  var fullName: String?=fullName?.toRegex(" ")
+        val parts: List<String>? = fullName?.split(" ")
         val firstName = parts?.getOrNull(0)
         val lastName = parts?.getOrNull(1)
-        println(firstName)
-        println(lastName)
 
-        return firstName to lastName
+
+        return stringOrNull(firstName) to stringOrNull(lastName.toString())
 
     }
 
@@ -24,18 +30,19 @@ object Utils {
             val letter = char.toString()
             if (letter == " ") {
                 stringTranslate = stringTranslate + divider
-            }else{
-            stringTranslate = stringTranslate + getTranlitLetter(letter)}
+            } else {
+                stringTranslate = stringTranslate + getTranlitLetter(letter)
+            }
         }
         return stringTranslate
     }
 
 
     fun toInitials(firstName: String?, lastName: String?): String? {
-       var strReturn:String ="${getFirstLetter(firstName)}${getFirstLetter(lastName)}"
-        strReturn=strReturn.replace(" ", "")
-       println(strReturn.length)
-        if (strReturn.length==0){
+        var strReturn: String = "${getFirstLetter(firstName)}${getFirstLetter(lastName)}"
+        strReturn = strReturn.replace(" ", "")
+        println(strReturn.length)
+        if (strReturn.length == 0) {
             return null
         }
         return strReturn
